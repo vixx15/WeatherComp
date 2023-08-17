5b30eca500ff14744f895871b6d5c9f46ff9cbc5
violeta.sekulic <violetas123321@gmail.com>
2023-08-17 15:17:10 +0200

    Zavrsena aplikacija

--- app/src/main/java/com/example/weathercomp/NavGraph.kt
+++ app/src/main/java/com/example/weathercomp/NavGraph.kt
@@ -7,12 +7,15 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 
 @Composable
-fun NavGraph(navController: NavHostController,viewModel: WeatherViewModel = androidx.lifecycle.viewmodel.compose.viewModel(),){
-    NavHost(navController = navController, startDestination = Screens.MainScreen.route){
-        composable(route=Screens.MainScreen.route){
-            MainScreen(viewModel=viewModel,navyController = navController)
+fun NavGraph(
+    navController: NavHostController,
+    viewModel: WeatherViewModel = androidx.lifecycle.viewmodel.compose.viewModel(),
+) {
+    NavHost(navController = navController, startDestination = Screens.MainScreen.route) {
+        composable(route = Screens.MainScreen.route) {
+            MainScreen(viewModel = viewModel, navyController = navController)
         }
-        composable(route=Screens.DetailScreen.route){
+        composable(route = Screens.DetailScreen.route) {
             WeatherDetails(viewModel = viewModel)
         }
 
